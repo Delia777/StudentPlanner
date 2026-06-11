e2c605bce1bb92e7c91839df57347d1df558155f
Kulcsar Delia <kulcsardelia77@gmail.com>
2026-06-11 21:57:53 +0300

    Remove autogenerated comments

--- ViewLocator.cs
+++ ViewLocator.cs
@@ -6,9 +6,7 @@ using StudentPlanner.ViewModels;
 
 namespace StudentPlanner;
 
-/// <summary>
-/// Given a view model, returns the corresponding view if possible.
-/// </summary>
+
 [RequiresUnreferencedCode(
     "Default implementation of ViewLocator involves reflection which may be trimmed away.",
     Url = "https://docs.avaloniaui.net/docs/concepts/view-locator")]
